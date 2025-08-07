42ba0a2000b298d24d459513ad4c9daad22c2a2c
Aditya Kumar <aditya.kumar2@happiestminds.com>
2025-08-07 12:48:55 +0530

    updated

--- electron-test-runner/Test-Foundry/GENAI_LLMJudge_ResponseQuality_Framework/Execute.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Activates the virtual environment
-call venv\Scripts\activate
-
-REM Runs the Python script
-python C:\GENAI_LLMJudge_ResponseQuality_Framework\src/main.py
-
-REM Keeps the command window open after execution
-exit
