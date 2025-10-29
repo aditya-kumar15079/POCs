c0eec712b329558d90731468591eb84d6a1f92b8
Aditya Kumar <aditya.kumar2@happiestminds.com>
2025-10-29 18:07:27 +0530

    react template moved out

--- FE-React-Template/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./reducers/counterSlice.js"; //it is the default export form counterSlice, we can name it anything.
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
